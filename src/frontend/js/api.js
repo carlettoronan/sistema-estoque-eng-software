658e4786fc72f0489123c2675a58f9973a9ec4c3
Ronan Carletto <carlettoronan@gmail.com>
2026-05-23 13:53:10 -0300

    Finaliza MVP: usuário e config do servidor

--- src/frontend/js/api.js
+++ src/frontend/js/api.js
@@ -1,2 +1,42 @@
-// Arquivo responsável por fazer as requisições (Fetch) para a API do Backend
-// Funções de comunicação serão implementadas aqui
+// Conexão com o servidor Flask
+const API_URL = "https://carlettoronan.pythonanywhere.com/api";
+
+const formLogin = document.getElementById("formLogin");
+if (formLogin) {
+    formLogin.addEventListener("submit", async (e) => {
+        e.preventDefault(); // Evita que a página recarregue
+        
+        const login = document.getElementById("login").value;
+        const senha = document.getElementById("senha").value;
+        const divErro = document.getElementById("mensagemErro");
+
+        try {
+            // Envia os dados para a API do Flask
+            const response = await fetch(`${API_URL}/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ login, senha })
+            });
+
+            const dados = await response.json();
+            
+            if (dados.sucesso) {
+                const nomeParam = encodeURIComponent(dados.usuario.nome);
+                const perfilParam = encodeURIComponent(dados.usuario.perfil);
+                
+                // Redirecionamento Inteligente baseado no Perfil
+                if (dados.usuario.perfil === "Admin") {
+                    window.location.href = `usuarios.html?nome=${nomeParam}&perfil=${perfilParam}`;
+                } else {
+                    window.location.href = `painel.html?nome=${nomeParam}&perfil=${perfilParam}`;
+                }
+            } else {
+                divErro.innerText = dados.mensagem;
+                divErro.classList.remove("d-none");
+            }
+        } catch (error) {
+            divErro.innerText = "Erro ao conectar com o servidor.";
+            divErro.classList.remove("d-none");
+        }
+    });
+}
